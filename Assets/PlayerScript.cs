be870c91b76fb798844392c187a9e57145d595fe
l4y4n <layanaloqda2003@gmail.com>
2024-02-19 20:49:40 +0300

    gameObject teaching class

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,12 +6,15 @@ public class PlayerScript : MonoBehaviour
 {
   public float speed = 1.5f;
     private Rigidbody2D playerRigidbody2D;
+    bool trigger = false;
+    bool bookTrigger = false;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
         playerRigidbody2D.gravityScale = 0; // Disable gravity for the main character
+
     }
 
     // Update is called once per frame
@@ -20,6 +23,42 @@ public class PlayerScript : MonoBehaviour
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         Vector2 moveVelocity = moveInput.normalized * speed;
         playerRigidbody2D.velocity = moveVelocity;
+        if(Input.GetKeyDown(KeyCode.X) && trigger) {
+            Debug.Log("press x");
+        }
+        if (Input.GetKeyDown(KeyCode.X) && bookTrigger)
+        {
+            Debug.Log("press X for book");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "key")
+        {
+            Debug.Log("its working");
+            trigger = true;
+        }
+        if(collision.gameObject.name == "Book")
+        {
+            Debug.Log("this is a book");
+            bookTrigger = true;
+        }
+
+       
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "key")
+        {
+            trigger = false;
+        }
+
+        if (collision.gameObject.name == "Book")
+        {
+            bookTrigger = false;
+        }
+
     }
 
 
